4480d4ffd65a44e5d6d27eae81e3ca6cb54da3ab
= <=>
2018-07-19 20:34:45 -0700

    experiment

--- src/main/java/com/shakepoint/web/api/resource/mapper/StandardExceptionMapper.java
+++ src/main/java/com/shakepoint/web/api/resource/mapper/StandardExceptionMapper.java
@@ -2,6 +2,7 @@ package com.shakepoint.web.api.resource.mapper;
 
 import com.shakepoint.web.api.data.dto.response.ErrorResponse;
 
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.ExceptionMapper;
 import javax.ws.rs.ext.Provider;
@@ -12,6 +13,7 @@ public class StandardExceptionMapper implements ExceptionMapper<Exception> {
     public Response toResponse(Exception e) {
         return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
                 .entity(new ErrorResponse("Ha ocurrido un error inesperado, intenta de nuevo"))
+                .type(MediaType.APPLICATION_JSON)
                 .build();
     }
 }
